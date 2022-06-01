2708356657a433380286be61164146d393c49dbf
Dylan097 <DylanAldridge7@gmail.com>
2022-06-01 20:43:44 +0000

    Create get_player_name function

--- run.py
+++ run.py
@@ -1,1 +1,10 @@
-print('hello world')
+def get_player_name():
+    """
+    Gets the name of the player
+    """
+    name = input('What is your name:\n')
+    print(f"Hello {name}, let's play some battleship!")
+    return name
+
+
+player_name = get_player_name()
