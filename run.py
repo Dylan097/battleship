1332d82f566c271f69b32cf2555c19d4a8cc7c2c
Dylan097 <DylanAldridge7@gmail.com>
2022-06-02 13:10:19 +0000

    Create GameBoard class

--- run.py
+++ run.py
@@ -1,5 +1,23 @@
 from random import randint
 
+scores = {'computer': 0, 'player': 0}
+
+
+class GameBoard:
+    """
+    Main board class. Sets the board size and the 
+    number of ships on the board, as well as the 
+    player's name and the board type (player or computer)
+    """
+    def __init__(self, size, num_ships, name, type):
+        self.size = size
+        self.board = [['.' for x in range(size)] for y in range(size)]
+        self.num_ships = num_ships
+        self.name = name
+        self.type = type
+        self.guesses = []
+        self.ships = []
+
 
 def get_player_name():
     """
@@ -10,14 +28,13 @@ def get_player_name():
     return name
 
 
-def get_board_size(name, section):
+def get_board_size(name):
     """
     Gets the board size the player would like to play with
     """
-    sect_size = input(f'What {section} size do you want to play with {name}\n')
-    return sect_size
+    size = input(f'What grid size do you want to play with {name}:\n')
+    return size
 
 
 player_name = get_player_name()
-row_size = get_board_size(player_name, 'row')
-col_size = get_board_size(player_name, 'column')
+board_size = get_board_size(player_name)
