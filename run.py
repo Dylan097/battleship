1c6b157aa74de82cad95b3a747869be895c1f60b
Dylan097 <DylanAldridge7@gmail.com>
2022-06-08 12:23:48 +0000

    Create guess and add_ship functions in GameBoard class

--- run.py
+++ run.py
@@ -18,10 +18,34 @@ class GameBoard:
         self.guesses = []
         self.ships = []
     
-
     def print(self):
         for row in self.board:
             print(' '.join(row))
+    
+    def guess(self, x, y):
+        self.guesses.append((x, y))
+        self.board[x][y] = 'X'
+
+        if (x, y) in self.ships:
+            self.board[x][y] = '*'
+            return 'Hit'
+        else:
+            return 'Miss'
+    
+    def add_ship(self, x, y, type='computer'):
+        if len(self.ships) >= self.num_ships:
+            print('Error: you cannot add anymore ships')
+        else:
+            self.ships.append((x, y))
+            if self.type == 'player':
+                self.board[x][y] = 'S'
+
+
+def random_point(size):
+    """
+    Returns a random integer between 0 and size
+    """
+    return randint(0, size - 1)
 
 
 def get_player_name():
